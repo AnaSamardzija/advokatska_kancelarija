2ac0e6901102dd7954f3ca72a20cb09e8cce0f70
AnaSamardzija <samardzija.ana06@gmail.com>
2023-06-04 11:30:27 +0200

    finish

---
+++ js/slideshow.js
@@ -0,0 +1,19 @@
+let slideIndex = 0;
+
+function Slides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }   
+    slides[slideIndex-1].style.display = "block"; 
+    setTimeout(Slides, 3000);
+}
+
+if(screen.width < 768){
+    Slides();
+}
