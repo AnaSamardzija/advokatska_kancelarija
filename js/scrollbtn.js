708da80387d512ba1a62a7b2c53ab5646c2965d1
AnaSamardzija <samardzija.ana06@gmail.com>
2023-06-07 15:01:17 +0200

    finish

---
+++ js/scrollbtn.js
@@ -0,0 +1,22 @@
+//dugme za scroll na pocetak stranice
+
+let btn_scrol = document.getElementById("scroll");
+
+//dugme se ne vidi, ali kada se kroluje 20px dugme postaje vidljivo
+function ScrollFunkcija() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        btn_scrol.style.display = "block";
+      } else {
+        btn_scrol.style.display = "none";
+      }
+}
+
+//kada se klikne na dugme, skroluje se na pocetak stranice
+btn_scrol.addEventListener("click", () => {
+    document.body.scrollTop = 0; //Safari
+    document.documentElement.scrollTop = 0; //Chrome, Firefox, Opera
+})
+
+window.onscroll = function() {
+    ScrollFunkcija()
+}
